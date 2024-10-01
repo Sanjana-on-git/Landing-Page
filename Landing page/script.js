7d2fd0c9ee1ccdee2aad0f50e1ee029246295c6b
Sanjana <235uit036@gbu.ac.in>
2024-10-01 06:38:48 +0530

    Add files via upload

---
+++ Landing page/script.js
@@ -0,0 +1,19 @@
+var i = 0;
+var txt = 'Hello'; /* The text */
+var speed = 50; 
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
